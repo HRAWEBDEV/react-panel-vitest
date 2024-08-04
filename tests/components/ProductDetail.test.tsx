9e2a0b4fa94a3bb0c612754841258d9b20115e9a
hamidwise1 <hrawebdev@gmail.com>
2024-08-04 18:38:22 +0330

    add faker and msw mock data and update server mock handlers

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,16 +1,30 @@
 import { screen, render } from '@testing-library/react';
 import ProductDetail from '../../src/components/ProductDetail';
-import { products } from '../mocks/data';
+import { server } from '../mocks/server';
+import { http, HttpResponse } from 'msw';
+import { db } from '../mocks/db';
 
 describe('productDetail', () => {
+ let productId: number;
+ beforeAll(() => {
+  const product = db.product.create();
+  productId = product.id;
+ });
+ afterAll(() => {
+  db.product.deleteMany({ where: { id: { equals: productId } } });
+ });
  it('should render the found product', async () => {
-  render(<ProductDetail productId={1} />);
-  expect(await screen.findByText(new RegExp(products[0].name)));
-  expect(await screen.findByText(new RegExp(products[0].price.toString())));
+  render(<ProductDetail productId={productId} />);
+  const product = db.product.findFirst({
+   where: { id: { equals: productId } },
+  });
+  expect(await screen.findByText(new RegExp(product!.name)));
+  expect(await screen.findByText(new RegExp(product!.price.toString())));
  });
 
  it('should render no product message if no product found ', async () => {
-  render(<ProductDetail productId={-1} />);
+  server.use(http.get('/products/:id', () => HttpResponse.json(null)));
+  render(<ProductDetail productId={4} />);
   const message = await screen.findByText(/not found/i);
   expect(message).toBeInTheDocument();
  });
--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,34 +1,5 @@
-import { http, HttpResponse } from 'msw';
-import { products } from './data';
+import { db } from './db';
 
-const handlers = [
- http.get('/categories', () => {
-  return HttpResponse.json([
-   {
-    id: 1,
-    name: 'beauty',
-   },
-   {
-    id: 2,
-    name: 'electronics',
-   },
-   {
-    id: 3,
-    name: 'gardening',
-   },
-  ]);
- }),
-
- http.get('/products', () => {
-  return HttpResponse.json(products);
- }),
-
- http.get('/products/:id', ({ params }) => {
-  const id = params.id as string;
-  const foundProduct = products.find((p) => p.id == parseInt(id));
-  if (!foundProduct) return HttpResponse.json(null, { status: 404 });
-  return HttpResponse.json(foundProduct);
- }),
-];
+const handlers = [...db.product.toHandlers('rest')];
 
 export { handlers };
